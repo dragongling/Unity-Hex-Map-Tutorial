d93e93a66c3008971c9b0fff474159357fb275a1
Alexander Slesarenko <dragongling@gmail.com>
2020-12-03 15:15:23 +0500

    Camera zoom implemented

---
+++ Assets/TopDownCamera.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    Transform swivel, stick;
+
+    public float stickMinZoom, stickMaxZoom;
+    public float swivelMinZoom, swivelMaxZoom;
+
+    float zoom = 1f;
+
+    private void Awake()
+    {
+        swivel = transform.GetChild(0);
+        stick = swivel.GetChild(0);
+    }
+
+    private void Update()
+    {
+        float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
+        if(zoomDelta != 0f)
+        {
+            AdjustZoom(zoomDelta);
+        }
+    }
+
+    private void AdjustZoom(float delta)
+    {
+        zoom = Mathf.Clamp01(zoom + delta);
+
+        float distance = Mathf.Lerp(stickMinZoom, stickMaxZoom, zoom);
+        stick.localPosition = new Vector3(0f, 0f, distance);
+
+        float angle = Mathf.Lerp(swivelMinZoom, swivelMaxZoom, zoom);
+        swivel.rotation = Quaternion.Euler(angle, 0f, 0f);
+    }
+}

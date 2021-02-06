f14679481d8d9467c9b4d97aba262c5807f7b8b7
Alexander Slesarenko <dragongling@gmail.com>
2021-02-06 18:15:48 +0500

    Refactoring

---
+++ Assets/ListPool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class ListPool<T> {
+    static Stack<List<T>> stack = new Stack<List<T>>();
+
+	public static List<T> Get()
+	{
+		if (stack.Count > 0)
+		{
+			return stack.Pop();
+		}
+		return new List<T>();
+	}
+
+	public static void Add(List<T> list)
+	{
+		list.Clear();
+		stack.Push(list);
+	}
+}

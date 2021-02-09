05f7f74f448f3f1296b8b3c0e3ed3811d59e0747
Alexander Slesarenko <dragongling@gmail.com>
2021-02-09 18:09:15 +0500

    Rivers tutorial complete

--- Assets/HexGrid/HexGridChunk.cs
+++ Assets/HexGrid/HexGridChunk.cs
@@ -120,7 +120,7 @@ public class HexGridChunk : MonoBehaviour
             e2.v3.y = neighbor.StreamBedY;
             TriangulateRiverQuad(
                 e1.v2, e1.v4, e2.v2, e2.v4, 
-                cell.RiverSurfaceY, neighbor.RiverSurfaceY, 
+                cell.RiverSurfaceY, neighbor.RiverSurfaceY, 0.8f,
                 cell.HasIncomingRiver && cell.IncomingRiver == direction);
         }
 
@@ -218,20 +218,20 @@ public class HexGridChunk : MonoBehaviour
         TriangulateEdgeFan(center, m, cell.Color);
 
         bool reversed = cell.HasIncomingRiver;
-        TriangulateRiverQuad(m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, reversed);
+        TriangulateRiverQuad(m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, 0.8f, reversed);
 
         center.y = m.v2.y = m.v4.y = cell.RiverSurfaceY;
         rivers.AddTriangle(center, m.v2, m.v4);
         if (reversed)
         {
             rivers.AddTriangleUV(
-                new Vector2(0.5f, 1f), new Vector2(1f, 0f), new Vector2(0f, 0f)
+                new Vector2(0.5f, 0.4f), new Vector2(1f, 0.2f), new Vector2(0f, 0.2f)
             );
         }
         else
         {
             rivers.AddTriangleUV(
-                new Vector2(0.5f, 0f), new Vector2(0f, 1f), new Vector2(1f, 1f)
+                new Vector2(0.5f, 0.4f), new Vector2(0f, 0.6f), new Vector2(1f, 0.6f)
             );
         }
     }
@@ -290,8 +290,8 @@ public class HexGridChunk : MonoBehaviour
         terrain.AddTriangleColor(cell.Color);
 
         bool reversed = cell.IncomingRiver == direction;
-        TriangulateRiverQuad(centerL, centerR, m.v2, m.v4, cell.RiverSurfaceY, reversed);
-        TriangulateRiverQuad(m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, reversed);
+        TriangulateRiverQuad(centerL, centerR, m.v2, m.v4, cell.RiverSurfaceY, 0.4f, reversed);
+        TriangulateRiverQuad(m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, 0.6f, reversed);
     }    
 
     private void TriangulateCorner(
@@ -484,21 +484,21 @@ public class HexGridChunk : MonoBehaviour
         terrain.AddQuadColor(c1, c2);
     }
 
-    void TriangulateRiverQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, float y, bool reversed)
+    void TriangulateRiverQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, float y, float v, bool reversed)
     {
-        TriangulateRiverQuad(v1, v2, v3, v4, y, y, reversed);
+        TriangulateRiverQuad(v1, v2, v3, v4, y, y, v, reversed);
     }
 
-    void TriangulateRiverQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, float y1, float y2, bool reversed) {
+    void TriangulateRiverQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, float y1, float y2, float v, bool reversed) {
         v1.y = v2.y = y1;
         v3.y = v4.y = y2;
         rivers.AddQuad(v1, v2, v3, v4);
         if (reversed) {
-            rivers.AddQuadUV(1f, 0f, 1f, 0f);
+            rivers.AddQuadUV(1f, 0f, 0.8f - v, 0.6f - v);
         }
         else
         {
-            rivers.AddQuadUV(0f, 1f, 0f, 1f);
+            rivers.AddQuadUV(0f, 1f, v, v + 0.2f);
         }
     }
 }
--- Assets/HexGrid/HexMetrics.cs
+++ Assets/HexGrid/HexMetrics.cs
@@ -35,7 +35,7 @@ public static class HexMetrics
 
     public const float selectionHeight = 1f;
 
-    public const float streamBedElevationOffset = -1f;
+    public const float streamBedElevationOffset = -1.75f;
 
     public const float riverSurfaceElevationOffset = -0.5f;
 
